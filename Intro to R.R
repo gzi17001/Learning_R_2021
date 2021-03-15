634f26550815e74e59f35137851b920a5c48043f
Grace_Iaconis <grace.iaconis@uconn.edu>
2021-03-15 15:29:51 -0400

    Learning R

---
+++ Intro to R.R
@@ -0,0 +1,4 @@
+365*24*60*60
+sqrt(78)
+rep("Hello!",100)
+hist(x=rnorm(300))
---
+++ Variables.R
@@ -0,0 +1,11 @@
+n_items <- 30
+item_time <- 25
+consent_time <- 120
+new_survey_time <- (n_items*item_time) + consent_time
+new_survey_time
+
+assigned_gender <- "female"
+identified_gender <- "non-binary"
+transgender <- assigned_gender == identified_gender
+
+
