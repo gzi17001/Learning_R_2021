54616919fcdd2a27f9a2df4444c8e6bacb7445b1
Grace_Iaconis <grace.iaconis@uconn.edu>
2021-03-22 18:09:33 -0400

    Packages and Workspaces

---
+++ Workspaces.R
@@ -0,0 +1,7 @@
+Fruit <- "Apple"
+Vegetable <- "Carrot"
+
+save.image("C:/Users/inven/OneDrive/Research/Learning_R_2021/Workspaces.RData")
+
+load("C:/Users/inven/OneDrive/Research/Learning_R_2021/Workspaces.RData")
+rm(Fruit)
