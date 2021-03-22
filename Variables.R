d4f03c903b26bae8f46a0d8b55e14df626d4fae6
Grace_Iaconis <grace.iaconis@uconn.edu>
2021-03-21 21:14:28 -0400

    new Variable

--- Variables.R
+++ Variables.R
@@ -1,9 +1,20 @@
+##Character Data
 n_items <- 30
 item_time <- 25
 consent_time <- 120
 new_survey_time <- (n_items*item_time) + consent_time
 new_survey_time
 
+##Logical Data Pt.1
 assigned_gender <- "female"
 identified_gender <- "non-binary"
 transgender <- assigned_gender != identified_gender
+
+##Logical Data Pt 2.
+# True
+status <- "married" 
+has_spouse <- (status == "married") | (status == "de facto")
+
+# False
+status <- "widowed" 
+has_spouse <- (status == "married") | (status == "de facto")
