05fc93bcb5894bc618dc5e5239a28de9a4f0bfb5
Grace_Iaconis <grace.iaconis@uconn.edu>
2021-03-26 03:12:05 -0400

    Opinions on seasons

---
+++ Branch.R
@@ -0,0 +1,26 @@
+s <- c("Spring","Summer","Fall","Winter","")
+
+for(season in s) {
+  if(season == "Spring") {
+    print("So many allergies")
+    
+  } else if(season == "Summer") {
+    print("Wonderful weather for a swim")
+    
+  } else if(season == "Fall") {
+    print("Sweater Weather")
+    
+  } else if(season == "Winter") {
+    print("Hot Chocolate weather")
+    
+  } else {
+    print("I don't have a preference")
+  }
+}  
+
+
+
+
+
+
+ 
